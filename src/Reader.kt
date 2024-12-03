e637d0712e923d213cf49c6e62d4b1d8f656d022
David Öhman <david.ohman@umain.com>
2024-12-03 09:45:16 +0100

    Day 03

--- src/Reader.kt
+++ src/Reader.kt
@@ -1,17 +1,15 @@
 object Reader {
-    fun example(path: String, split: String = "\n") =
+    fun example(path: String, split: String? = "\n") =
         lines("/$path/example", split)
 
-    fun input(path: String, split: String = "\n") =
+    fun input(path: String, split: String? = "\n") =
         lines("/$path/input", split)
 
-    private fun lines(path: String, split: String) =
+    private fun lines(path: String, split: String?): List<String> =
         this@Reader::class.java
             .getResource(path)!!
             .readText()
-            .split(split)
-            .map(String::trim)
-            .superTrim()
+            .run { if (split == null) listOf(this) else split(split).map(String::trim).superTrim() }
 
     private fun List<String>.superTrim(): List<String> {
         val trimmedList = mutableListOf<String>()
--- src/d03.kt
+++ src/d03.kt
@@ -1,12 +1,34 @@
+const val DO = "do()"
+const val DONT = "don't()"
+
 fun main(args: Array<String>) {
     d03a()
     d03b()
 }
 
 fun d03a() {
-    val lines = Reader.input("d03")
+    val sum = Reader.input("d03", split = null)[0].mul()
+    require(sum == 157621318)
 }
 
 fun d03b() {
-    val lines = Reader.input("d03")
+    var line = Reader.input("d03", split = null)[0]
+    var enabled = ""
+
+    while (line.contains(DONT)) {
+        enabled += line.substringBefore(DONT)
+        line = line
+            .substringAfter(DONT)
+            .substringAfter(DO, missingDelimiterValue = "")
+    }
+
+    val sum = enabled.mul()
+    require(sum == 79845780)
 }
+
+private fun String.mul(): Int =
+    let { Regex("""mul\(\d+,\d+\)""").findAll(it).map(MatchResult::value) }
+        .sumOf {
+            val (a, b) = it.substringAfter("(").substringBefore(")").split(",").map(String::toInt)
+            a * b
+        }
